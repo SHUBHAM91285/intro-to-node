8659b6ffcb7f4e4c165e13b3a7aed15a291bae84
SHUBHAM91285 <tshubhamnath@gmail.com>
2022-09-08 01:52:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var superheroes = require("superheroes");
+
+var mySuperhero = superheroes.random();
+
+console.log(mySuperhero);
+
+var supervillian = require("supervillains");
+
+var mySupervillian = supervillian.random();
+
+console.log(mySupervillian);
